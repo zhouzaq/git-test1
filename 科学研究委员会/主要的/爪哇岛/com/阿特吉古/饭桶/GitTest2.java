238654930ff3ff0195fabd3813a355e1686e59c7
zhouzaq <114329163+zhouzaq@users.noreply.github.com>
2022-09-25 18:15:51 +0800

    pull commit

--- 科学研究委员会/主要的/爪哇岛/com/阿特吉古/饭桶/GitTest2.java
+++ 科学研究委员会/主要的/爪哇岛/com/阿特吉古/饭桶/GitTest2.java
@@ -13,7 +13,7 @@ public class GitTest2 {
         System.out.println("master test!");
         System.out.println("hot-fix test");
         System.out.println("push test!");
-         System.out.println("pull test!");
+         System.out.println("pull test2!");
         
     }
 }
