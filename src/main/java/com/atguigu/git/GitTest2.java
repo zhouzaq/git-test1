6ec55fda68b427e2a9d248437e11f8217650a898
Layne <Layne@atguigu.com>
2022-09-25 16:50:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zhou
+ * @create 2022-09-25 16:47
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("你好，Git");
+    }
+}
