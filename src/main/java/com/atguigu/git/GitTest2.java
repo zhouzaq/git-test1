412cc32b9f1316a9a3b8dd9605b1ec8d224088db
Layne <Layne@atguigu.com>
2022-09-25 16:57:11 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest2.java
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -8,5 +8,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("你好，Git");
         System.out.println("hello,git2!");
+        System.out.println("hello,git3");
     }
 }
