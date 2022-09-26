b63c6643235dc7c9ec9375a404044f04b303acb3
Layne <Layne@atguigu.com>
2022-09-26 14:19:23 +0800

    english commit2

--- src/main/java/com/atguigu/git/GitTest2.java
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -14,6 +14,7 @@ public class GitTest2 {
         System.out.println("hot-fix test");
         System.out.println("push test!");
          System.out.println("pull test2!");
+        System.out.println("修改完成");
         
     }
 }
